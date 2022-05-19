21ea7e380711f56622123b7c1bdc5bba55713569
deivid-a1 <deivid_506_02@hotmail.com>
2022-05-19 19:03:16 -0300

    detecting with coco .pb in only one image

--- opencv/onlyImageDetection.cpp
+++ opencv/onlyImageDetection.cpp
@@ -8,6 +8,7 @@
 #include <X11/Xutil.h>
 
 #include <time.h>
+#include <fstream>
 // #define FPS(start) (CLOCKS_PER_SEC / (clock()-start))
 
 using namespace cv;
@@ -18,37 +19,50 @@ using namespace dnn;
 int main()
 {
 
-    vector<string> class_names = {"sign_red", "sign_green", "sign_yellow"};
+    vector<string> class_names;
 
+    ifstream ifs(string("caffe/object_detection_classes_coco.txt").c_str());
+    string line;
 
-    auto model = readNetFromTensorflow("tensor/peso.pb","tensor/algo.txt");
-    model.setPreferableTarget(DNN_TARGET_OPENCL);
+    while (getline(ifs, line))
+    {
+        cout << line << endl;
+        class_names.push_back(line);
+    } 
+
+    auto model = readNet("caffe/frozen_inference_graph.pb","caffe/ssd_mobilenet_v2_coco_2018_03_29.pbtxt.txt", "TensorFlow");
+    // model.setPreferableTarget(DNN_TARGET_OPENCL);
 
     Mat image = imread("dumpImageTest/bunchTraffic.png");
-
-    
 
     int image_height = image.cols;
     int image_width = image.rows;
     
-    getTickCount();
     
-    Mat blob = blobFromImage(image, 0.00392, Size(300, 300), Scalar(127.5, 127.5, 127.5), true, false);
+    auto start = getTickCount();
+    
+    Mat blob = blobFromImage(image, 1.0, Size(300, 300), Scalar(127.5, 127.5, 127.5), true, false);
+    
     
 
     model.setInput(blob);
-    
+
+
     
     Mat output = model.forward();
+
+
 
     auto end = getTickCount();
 
     Mat detectionMat(output.size[2], output.size[3], CV_32F, output.ptr<float>());
 
 
+    
     for (int i = 0; i < detectionMat.rows; i++){
-       int class_id = detectionMat.at<float>(i, 1);
-       float confidence = detectionMat.at<float>(i, 2);
+        int class_id = int(detectionMat.at<float>(i, 1));
+        float confidence = detectionMat.at<float>(i, 2);
+
       
        // Check if the detection is of good quality
        if (confidence > 0.4){
@@ -56,7 +70,7 @@ int main()
            int box_y = static_cast<int>(detectionMat.at<float>(i, 4) * image.rows);
            int box_width = static_cast<int>(detectionMat.at<float>(i, 5) * image.cols - box_x);
            int box_height = static_cast<int>(detectionMat.at<float>(i, 6) * image.rows - box_y);
-           rectangle(image, Point(box_x, box_y), Point(box_x+box_width, box_y+box_height), Scalar(255,255,255), 2);
+           rectangle(image, Point(box_x, box_y), Point(box_x+box_width, box_y+box_height), Scalar(211,0,148), 2);
            putText(image, class_names[class_id-1].c_str(), Point(box_x, box_y-5), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0,255,255), 1);
        }
    } 
